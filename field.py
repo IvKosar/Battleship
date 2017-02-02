ac5afd4a27c7d33177cc432b89fc196acb4dd782
Marian Petruk <marian.petruk@gmail.com>
2017-02-02 13:30:06 +0200

    start

---
+++ field.py
@@ -0,0 +1,38 @@
+
+
+def read_field():
+    """
+    (str) -> (data)
+    """
+    pass
+
+def has_ship():
+    """
+    (data, tuple) -> (bool)
+    """
+    pass
+
+def ship_size():
+    """
+    (data, tuple) -> (tuple)
+    """
+    pass
+
+def is_valid():
+    """
+    (data) -> (bool)
+    """
+    pass
+
+def field_to_str():
+    """
+    (data) -> (str)
+    """
+    pass
+
+def generate_field():
+    """
+    () -> (data)
+    """
+    pass
+
